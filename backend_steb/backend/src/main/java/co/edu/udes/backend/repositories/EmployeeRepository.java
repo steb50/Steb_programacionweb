6450941788471b5390a1bf5038e8d984b7e19a99
steb50 <64308350+steb50@users.noreply.github.com>
2024-09-13 17:58:14 -0500

    Add files via upload

---
+++ backend_steb/backend/src/main/java/co/edu/udes/backend/repositories/EmployeeRepository.java
@@ -0,0 +1,11 @@
+package co.edu.udes.backend.repositories;
+
+import co.edu.udes.backend.models.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+
+
+}
